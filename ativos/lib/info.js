acdef8fd5af972096c0540a7a1d3d9f811273f97
WHITEZY07 <80177129+WHITEZY07@users.noreply.github.com>
2021-03-06 18:23:16 -0300

    Add files via upload

---
+++ ativos/lib/info.js
@@ -0,0 +1,44 @@
+exports.info = (id, A187, tampilTanggal, whatsapp, youtube, tampilWaktu, instagram, nomer, aktif) => {
+	return ` 
+❉──────────❉
+
+*${A187}*
+
+❉──────────❉
+
+📆 *${tampilTanggal}*
+📌STATUS BOT ATIVO: *${aktif}*
+
+         
+╔════════════════════
+║ *Feito por: $Whitezy*
+╠════════════════════
+║   *⚠️ATENÇÃO⚠️*
+╠════════════════════
+║*OS CRÉDITOS NAO SÃO MEUS*
+║*APENAS FIZ UMA VERSÃO INSTÁVEL*
+╠════════════════════
+║╭──❉ *INFO* ❉──
+║│➸ *AUTOR*: CHRISTOFER
+║│➸ *DESIGNER*: CHRISTOFER
+║│➸ *YOUTUBE*: WHITEZY
+║╰───────────
+╠════════════════════
+║╭──❉ *SOSMED ADMIN* ❉──
+║│➸ *Faça login aqui😙*
+║│ _${whatsapp}_
+║│➸ *YouTube*
+║│ _${nomer}_
+║│➸ *Instagram <Segue>*
+║│ _${instagram}_
+║│➸ *Criador*
+║│    WHITEZY
+║╰───────────
+║         ${A187}
+║  ▌│█║▌║▌║║▌║▌║█│▌
+║  ▌│█║▌║▌║║▌║▌║█│▌
+║         ${A187}
+╠════════════════════
+║ _*Feito por WHITEZY*_
+╚════════════════════`
+}
